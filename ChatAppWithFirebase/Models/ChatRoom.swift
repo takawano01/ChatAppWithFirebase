09e54f68334abb9eed8e8ffbd93d18b0c9a33b9a
河野隆 <takawano@kounotakashi-no-MacBook-Pro.local>
2020-09-30 10:20:59 +0900

    add

---
+++ ChatAppWithFirebase/Models/ChatRoom.swift
@@ -0,0 +1,9 @@
+//
+//  ChatRoom.swift
+//  ChatAppWithFirebase
+//
+//  Created by 河野隆 on 2020/09/25.
+//  Copyright © 2020 河野隆. All rights reserved.
+//
+
+import Foundation
---
+++ ChatAppWithFirebase/Models/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  ChatAppWithFirebase
+//
+//  Created by 河野隆 on 2020/09/27.
+//  Copyright © 2020 河野隆. All rights reserved.
+//
+
+import Foundation
